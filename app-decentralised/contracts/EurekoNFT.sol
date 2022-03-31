cdba317fac716b0a05810479b3cf9e8654bc0e5f
NG Rui Qin <ruiqin.ng.2016@sis.smu.edu.sg>
2022-03-31 23:45:43 +0800

    Update contract

--- app-decentralised/contracts/EurekoNFT.sol
+++ app-decentralised/contracts/EurekoNFT.sol
@@ -3,10 +3,11 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/interfaces/IERC2981.sol";
 
-contract EurekoNFT is ERC721, Ownable, IERC2981 {
+contract EurekoNFT is ERC721Enumerable, Ownable, IERC2981 {
     mapping(uint256 => string) private _tokenURIs; // Token id to token URI
     address private _owner;
 
@@ -18,18 +19,18 @@ contract EurekoNFT is ERC721, Ownable, IERC2981 {
         _owner = owner;
     }
 
-    function mint(address recipient, uint256 tokenId, string memory uri)
+    function mint(address recipient, uint256 tokenId) public
         returns (uint256)
     {
         require(isReleased, "COLLECTION_NOT_RELEASED");
-        require(!_exists(tokenId), "TOKEN_MINTED");
-        require(_tokenURIs[tokenId] != "", "TOKEN_NOT_FOUND");
+        require(bytes(_tokenURIs[tokenId]).length > 0, "TOKEN_NOT_FOUND");
+        require(_exists(tokenId), "TOKEN_MINTED");
         
         _safeMint(recipient, tokenId);
         return tokenId; // Need to return id?
     }
 
-    function royaltyInfo(uint256 tokenId, uint256 salePrice) external view
+    function royaltyInfo(uint256, uint256 salePrice) external view override(IERC2981)
         returns (address, uint256)
     {
         return(_owner, (salePrice / 100) * 6);
@@ -38,6 +39,12 @@ contract EurekoNFT is ERC721, Ownable, IERC2981 {
     function toggleRelease() external onlyOwner {
         // Release/Lock NFT collection for minting
         isReleased = !isReleased;
+    }
+
+    function release() public view
+        returns (bool)
+    {
+        return isReleased;
     }
 
     function setTokenURI(uint256 tokenId, string calldata uri) external onlyOwner {
@@ -51,8 +58,21 @@ contract EurekoNFT is ERC721, Ownable, IERC2981 {
         return string(abi.encodePacked(_tokenURIs[tokenId]));
     }
 
-    function redeem(address from, uint256 tokenId) {
-        safeTransferFrom(from, _owner, tokenId);
+    function redeem(address owner, uint256 tokenId) public
+        returns (bool)
+    {
+        uint256 num_tokens = balanceOf(owner);
+        require(num_tokens > 0, "NO_TOKEN_IN_POSSESSION");
+
+        for (uint256 idx = 0; idx < num_tokens; idx++) {
+            uint256 tokenToCompare = tokenOfOwnerByIndex(owner, idx);
+            if (tokenToCompare == tokenId) {
+                _burn(tokenId);
+                return true;
+            }
+        }
+        
+        return false;
     }
     
 }
