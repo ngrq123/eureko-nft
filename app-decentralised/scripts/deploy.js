f594dc3583d795eb3cf65abf91336faa7bd68ee5
NG Rui Qin <ruiqin.ng.2016@sis.smu.edu.sg>
2022-03-31 18:26:01 +0800

    Add constructor arg to deploy.js

--- app-decentralised/scripts/deploy.js
+++ app-decentralised/scripts/deploy.js
@@ -1,8 +1,10 @@
+const OWNER_ADDRESS = process.env.PUBLIC_KEY;
+
 async function main() {
   const EurekoNFT = await ethers.getContractFactory("EurekoNFT")
 
   // Start deployment, returning a promise that resolves to a contract object
-  const eurekoNFT = await EurekoNFT.deploy()
+  const eurekoNFT = await EurekoNFT.deploy(OWNER_ADDRESS) // https://ethereum.stackexchange.com/a/30766
   await eurekoNFT.deployed()
   console.log("Contract deployed to address:", eurekoNFT.address)
 }
