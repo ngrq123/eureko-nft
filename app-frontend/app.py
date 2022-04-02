ad9fb656469caa846c5c8403fba12dd9d739e81e
NG Rui Qin <ruiqin.ng.2016@sis.smu.edu.sg>
2022-04-03 04:47:34 +0800

    Update frontend visuals

--- app-frontend/app.py
+++ app-frontend/app.py
@@ -1,7 +1,9 @@
+from dotenv import load_dotenv
 import json
 import os
-from dotenv import load_dotenv
+import time
 
+import requests
 import streamlit as st
 from web3.auto.gethdev import w3
 from web3 import Web3, IPCProvider
@@ -17,12 +19,15 @@ w3 = Web3(Web3.HTTPProvider('https://eth-rinkeby.alchemyapi.io/v2/1QFGnc3guOeSJ7
 w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
 abi = json.loads(ABI_JSON)
-print(abi)
 
 nftContract = w3.eth.contract(address=CONTRACT_ADDRESS, abi=abi)
 
 def mint(from_address, token_id):
-  nonce = w3.eth.get_transaction_count(from_address, 'latest')
+  if len(from_address) == 0:
+    st.error('Please enter your wallet address in the sidebar')
+    return
+
+  nonce = w3.eth.get_transaction_count(PUBLIC_KEY, 'latest')
 
   txn = nftContract.functions.mint(from_address, token_id).buildTransaction({
     'nonce': nonce,
@@ -31,9 +36,33 @@ def mint(from_address, token_id):
   
   signed_txn = w3.eth.account.sign_transaction(txn, private_key=PRIVATE_KEY)
   w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+  st.info('The hash of your transaction is: ' + signed_txn.hash.hex())
+
+  for _ in range(12):
+    try:
+      res = w3.eth.get_transaction_receipt(signed_txn.hash.hex())
+      while 'status' not in res:
+        time.sleep(5)
+        res = w3.eth.get_transaction_receipt(signed_txn.hash.hex())
+
+      status = res['status']
+
+      if status == 1:
+        st.success('Transaction successful')
+        break
+      else:
+        st.error('Transaction failed')
+        break
+    except:
+      time.sleep(5)
+      continue
 
 
 def redeem(from_address, token_id):
+  if len(from_address) == 0:
+    st.error('Please enter your wallet address in the sidebar')
+    return
+  
   nonce = w3.eth.get_transaction_count(PUBLIC_KEY, 'latest')
 
   txn = nftContract.functions.redeem(from_address, token_id).buildTransaction({
@@ -43,6 +72,26 @@ def redeem(from_address, token_id):
   
   signed_txn = w3.eth.account.sign_transaction(txn, private_key=PRIVATE_KEY)
   w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+  st.info('The hash of your transaction is: ' + signed_txn.hash.hex())
+
+  for _ in range(12):
+    try:
+      res = w3.eth.get_transaction_receipt(signed_txn.hash.hex())
+      while 'status' not in res:
+        time.sleep(5)
+        res = w3.eth.get_transaction_receipt(signed_txn.hash.hex())
+
+      status = res['status']
+
+      if status == 1:
+        st.success('Transaction successful')
+        break
+      else:
+        st.error('Transaction failed')
+        break
+    except:
+      time.sleep(5)
+      continue
 
 # Dropdown box
 selected_function = st.sidebar.selectbox('Select Function', 
@@ -55,8 +104,20 @@ selected_function = st.sidebar.selectbox('Select Function',
 # Text box
 from_address = st.sidebar.text_input('Enter Wallet Address')
 
+# Number input box
+token_id = st.sidebar.number_input('Enter token id', min_value=1, step=1)
+
+st.title('Eureko')
+
 if selected_function == 'Mint':
-  st.button('Mint NFT', on_click=mint, args=(from_address, 13))
+  st.image('./images/token_unrevealed.png')
+  st.button('Mint NFT', on_click=mint, args=(from_address, token_id))
 
 if selected_function == 'Redeem':
-  st.button('Redeem NFT', on_click=redeem, args=(from_address, 13))
+  try:
+    metadata_url = nftContract.functions.tokenURI(16).call()
+    res = requests.get(metadata_url).json()
+    st.image(res['image'])
+    st.button('Redeem NFT', on_click=redeem, args=(from_address, token_id))
+  except:
+    st.error('Token not exists - token is not in circulation')
